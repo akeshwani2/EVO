5c502f949b464b5f3cf484397186a557d988cdac
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2025-01-18 15:11:33 -0500

    Minor Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,57 +19,59 @@ export default function Home() {
           </h1>
           <div className="flex flex-col items-center gap-4">
             {/* Description */}
-            <p className="text-md text-gray-400 max-w-[600px] md:text-lg/relaxed xl:text-2xl/relaxed tracking-tight">
-              Meet EVO, an AI agent that goes beyond the ordinary
+            <p className="text-sm text-center text-gray-400 max-w-[600px] md:text-lg/relaxed xl:text-xl/relaxed tracking-tight">
+              EVO is an AI agent that goes beyond the ordinary, it is your personal assistant that can help you with your daily tasks
             </p>
             <Link href="https://wxflows.ibm.stepzen.com">
-              <span className="text-gray-500 text-xs sm:text-sm md:text-base lg:text-lg xl:text-lg border border-gray-400 rounded-full py-1 px-3 flex items-center">
+              <span className="text-gray-500 text-sm text-center sm:text-sm md:text-base lg:text-md xl:text-md border border-gray-400 rounded-full py-1 px-3 flex items-center pl-1">
                 <Dot className="w-8 h-8 text-white font-bold animate-pulse" />
-                Powered by IBM&apos;s WxTools along with your favorite LLMs
+                Powered by IBM WxTools & LLMs
               </span>
             </Link>
           </div>
         </header>
 
-        {/* CTA Button */}
-        <div className="flex items-center gap-4">
+        {/* CTA Buttons */}
+        <div className="flex flex-col items-center gap-4">
           <SignedIn>
-            <Link href="/dashboard">
-            <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
-                Chat with EVO
-                <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
-              </button>
-            </Link>
-            <Link href="/docs">
-            <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
-                Learn More
-                <ArrowUpRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
-              </button>
-            </Link>
+            <div className="flex items-center gap-4">
+              <Link href="/dashboard">
+                <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
+                  Chat with EVO
+                  <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
+                </button>
+              </Link>
+              <Link href="/docs">
+                <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
+                  Learn More
+                  <ArrowUpRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
+                </button>
+              </Link>
+            </div>
           </SignedIn>
-        </div>
-        <div className="flex items-center gap-4">
           <SignedOut>
-            <SignInButton
-              mode="modal"
-              fallbackRedirectUrl={"/dashboard"}
-              forceRedirectUrl={"/dashboard"}
-            >
-              <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
-                Sign Up
-                <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
-              </button>
-            </SignInButton>
-            <Link href="/docs">
-            <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
-                Learn More
-                <ArrowUpRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
-              </button>
-            </Link>
+            <div className="flex items-center gap-4">
+              <SignInButton
+                mode="modal"
+                fallbackRedirectUrl={"/dashboard"}
+                forceRedirectUrl={"/dashboard"}
+              >
+                <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
+                  Sign Up
+                  <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
+                </button>
+              </SignInButton>
+              <Link href="/docs">
+                <button className="group relative inline-flex items-center justify-center px-4 py-3.5 text-base font-medium text-black bg-white rounded-lg hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:-translate-y-0.5 tracking-tighter">
+                  Learn More
+                  <ArrowUpRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-gray-900/20 to-gray-800/20 blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
+                </button>
+              </Link>
+            </div>
           </SignedOut>
         </div>
 
