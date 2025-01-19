39eab6aad8841280e3a2bd3d012f64f5bdedba5a
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2025-01-19 10:28:04 -0500

    Starting on chatId page

--- src/components/ui/ChatRow.tsx
+++ src/components/ui/ChatRow.tsx
@@ -2,6 +2,7 @@ import { useNavigation } from "@/lib/context/navigation";
 import { Doc, Id } from "../../../convex/_generated/dataModel";
 import { useRouter } from "next/navigation";
 import { Trash } from "lucide-react";
+import { Button } from "./button";
 
 function ChatRow({
     chat,
@@ -25,13 +26,18 @@ function ChatRow({
     };
 
     return (
-        <div className="text-white border flex items-center justify-between border-white/60 rounded-lg px-4 py-2 hover:bg-white/10 transition-colors cursor-pointer" onClick={handleClick}>
-            <p>{chat.title}</p>
-            <Trash 
-                className="w-4 h-4 text-white/60 hover:text-white transition-colors cursor-pointer" 
-                onClick={handleDelete} 
-            />
+        <div className="text-white border flex items-center justify-between border-white/40 tracking-tight rounded-lg px-4 py-2 hover:bg-white/10 cursor-pointer group" onClick={handleClick}>
+            <p className="text-md">{chat.title}</p>
+            <Button 
+                variant="ghost" 
+                size="icon" 
+                onClick={handleDelete}
+                className="hover:white hover:text-black"
+            >
+                <Trash className="w-2 h-2 md:opacity-0 md:group-hover:opacity-100 cursor-pointer" />
+            </Button>
         </div>
     )
+    
 }
 export default ChatRow
